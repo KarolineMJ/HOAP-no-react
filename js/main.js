640ba14e32edb1c96c84cc976b62bf5dc603408c
KarolineMJ <karoline_mj@hotmail.com>
2018-12-03 13:51:37 +0100

    Added: addPetForm - linked to database

--- js/main.js
+++ js/main.js
@@ -15,6 +15,32 @@ function renderAnimal() {
   console.log("Hi Yan's animals");
 }
 
+const addPetForm = document.querySelector("#addPetForm");
+
+//save data to firebase
+addPetForm.addEventListener("submit", e => {
+  e.preventDefault();
+  console.log(addPetForm.morning.checked);
+  db.collection("animals").add({
+    name: addPetForm.name.value,
+    gender: addPetForm.gender.value,
+    type: addPetForm.type.value,
+    race: addPetForm.race.value,
+    age: addPetForm.age.value,
+    story: addPetForm.story.value,
+    size: addPetForm.size.value,
+    pregnant: addPetForm.pregnant.checked,
+    pup: addPetForm.pup.checked,
+    activity: [
+      { morning: addPetForm.morning.checked },
+      { afternoon: addPetForm.afternoon.checked },
+      { evening: addPetForm.evening.checked },
+      { extratraining: addPetForm.extra.checked }
+    ],
+    foodneed: addPetForm.foodneed.value
+  });
+});
+
 const db = firebase.firestore();
 
 //get data from firebase
