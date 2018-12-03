64aeab158710dc4238c0a519bcc5b587288ffbf4
KarolineMJ <karoline_mj@hotmail.com>
2018-12-03 14:48:53 +0100

    Added: reading from database

--- js/main.js
+++ js/main.js
@@ -11,8 +11,42 @@ let config = {
 };
 firebase.initializeApp(config);
 
-function renderAnimal() {
-  console.log("Hi Yan's animals");
+const animalList = document.querySelector("#animalList");
+const animalTemp = document.querySelector("#animalTemp").content;
+
+function renderAnimal(doc) {
+  const clone = animalTemp.cloneNode(true);
+  clone.querySelector(".petName").textContent = doc.data().name;
+  clone.querySelector(".petType").textContent = doc.data().type;
+  clone.querySelector(".petAge span").textContent = doc.data().age;
+  clone.querySelector(".petGender").textContent = doc.data().gender;
+  clone.querySelector(".petRace").textContent = doc.data().race;
+  clone.querySelector(".petStory").textContent = doc.data().story;
+  clone.querySelector(".petSize").textContent = doc.data().size;
+  clone.querySelector(".petPregnant").textContent = doc.data().pregnant
+    ? "Pregnant"
+    : "";
+  clone.querySelector(".petPup").textContent = doc.data().pup ? "Pup" : "";
+  clone.querySelector(".petActivity li:nth-of-type(1)").textContent = doc.data()
+    .activity[0].morning
+    ? "Morning"
+    : "x";
+  clone.querySelector(".petActivity li:nth-of-type(2)").textContent = doc.data()
+    .activity[1].afternoon
+    ? "Afternoon"
+    : "x";
+  clone.querySelector(".petActivity li:nth-of-type(3)").textContent = doc.data()
+    .activity[2].evening
+    ? "Evening"
+    : "x";
+  clone.querySelector(".petActivity li:nth-of-type(4)").textContent = doc.data()
+    .activity[3].extratraining
+    ? "Extra"
+    : "x";
+
+  clone.querySelector(".petFoodneed").textContent = doc.data().foodneed + "kr";
+
+  animalList.appendChild(clone);
 }
 
 const addPetForm = document.querySelector("#addPetForm");
@@ -23,10 +57,10 @@ addPetForm.addEventListener("submit", e => {
   console.log(addPetForm.morning.checked);
   db.collection("animals").add({
     name: addPetForm.name.value,
-    gender: addPetForm.gender.value,
     type: addPetForm.type.value,
-    race: addPetForm.race.value,
+    gender: addPetForm.gender.value,
     age: addPetForm.age.value,
+    race: addPetForm.race.value,
     story: addPetForm.story.value,
     size: addPetForm.size.value,
     pregnant: addPetForm.pregnant.checked,
