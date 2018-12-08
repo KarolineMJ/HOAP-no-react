148874d1b8aabd5c02215052256825ff36615bd5
KarolineMJ <karoline_mj@hotmail.com>
2018-12-08 22:22:28 +0100

    Added: Form animation

--- js/frontpage.js
+++ js/frontpage.js
@@ -1,18 +1,28 @@
 "use strict";
-let placeholders = document.querySelectorAll("input").placeholder;
 
-function checkForPlaceholder() {
-  if (placeholders !== "") {
-    console.log("there is something");
-    placeholders = "hi mom";
-  }
-}
-checkForPlaceholder();
+/*-------------------------------------------
+Display signin form
+------------------------------------------*/
 
 let memberBtn = document.querySelector("#alreadyMemberBtn");
-let signinForm = document.querySelector(".loginForm");
+let signinForm = document.querySelector("#loginForm");
 memberBtn.addEventListener("click", openSigninForm);
 
 function openSigninForm() {
   signinForm.classList.toggle("visible");
 }
+
+/*-------------------------------------------
+Open Modal
+------------------------------------------*/
+/*let inputfield = document.querySelectorAll("input");
+let label = document.querySelectorAll("label");
+
+inputfield.forEach(inputfield => {
+  if (inputfield.placeholder !== "") {
+    console.log("there is something");
+  } else {
+    console.log("there is nothing");
+  }
+});
+*/
