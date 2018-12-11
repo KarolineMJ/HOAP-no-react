4bd71983e63ae03230ac5d4dda703e6d25b8d3ab
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-12-11 16:54:36 +0100

    Added: show only hearted and order by

--- js/menu.js
+++ js/menu.js
@@ -4,10 +4,26 @@ Show/hide sidemenu
 
 function hideMenu() {
   const settingBtn = document.querySelector("#settingBtn");
+  const newsBtn = document.querySelector("#newsBtn");
   const sidebar = document.querySelector(".sidebar");
   const signedInContainer = document.querySelector(".signedInContainer");
 
   settingBtn.addEventListener(
+    "click",
+    function() {
+      console.log("clicked");
+      if (sidebar.style.display == "none") {
+        sidebar.style.display = "block";
+        signedInContainer.classList.remove("closed");
+      } else {
+        sidebar.style.display = "none";
+        signedInContainer.classList.add("closed");
+      }
+    },
+    false
+  );
+
+  newsBtn.addEventListener(
     "click",
     function() {
       console.log("clicked");
