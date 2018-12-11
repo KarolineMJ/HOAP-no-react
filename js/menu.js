5754923ce0007116730c346bddade50225038dbe
Tiril Grimsrud <tiril_k_g@hotmail.com>
2018-12-11 19:47:23 +0100

    Added: click events on images

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+"use strict";
+
 /*-------------------------------------------
 Show/hide sidemenu
 ------------------------------------------*/
@@ -8,20 +10,16 @@ function hideMenu() {
   const sidebar = document.querySelector(".sidebar");
   const signedInContainer = document.querySelector(".signedInContainer");
 
-  settingBtn.addEventListener(
-    "click",
-    function() {
-      console.log("clicked");
-      if (sidebar.style.display == "none") {
-        sidebar.style.display = "block";
-        signedInContainer.classList.remove("closed");
-      } else {
-        sidebar.style.display = "none";
-        signedInContainer.classList.add("closed");
-      }
-    },
-    false
-  );
+  settingBtn.addEventListener("click", function() {
+    console.log("clicked");
+    if (sidebar.style.display == "none") {
+      sidebar.style.display = "block";
+      signedInContainer.classList.remove("closed");
+    } else {
+      sidebar.style.display = "none";
+      signedInContainer.classList.add("closed");
+    }
+  });
 
   newsBtn.addEventListener(
     "click",
@@ -39,3 +37,16 @@ function hideMenu() {
   );
 }
 hideMenu();
+
+function expandInfo() {
+  const petImage = document.querySelectorAll("#animalImage img");
+
+  for (let i = 0; i < petImage.length; i++) {
+    petImage[i].addEventListener("click", function() {
+      console.log("clicked");
+      petImage[i].classList.toggle("done");
+    });
+  }
+}
+
+expandInfo();
