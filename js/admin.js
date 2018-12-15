098739ed62a8a613b4357668820669cfb06ad3ac
xiaobaoxixi <liyan.xiaobaoxixi@gmail.com>
2018-12-15 11:36:24 +0100

    Try out 2 people working on one branch

--- js/admin.js
+++ js/admin.js
@@ -15,6 +15,8 @@ const deleteAnimalBtn = document.querySelector(".deleteAnimal");
 const animalDetailModal = document.querySelector(".animalDetailModal");
 const animalDetailForm = document.querySelector(".animalDetails");
 const date = document.querySelector(".date");
+const animalListOnLoggedIn = document.querySelector("#animalList");
+
 // date related
 const today = new Date();
 const year = today.getFullYear();
@@ -209,6 +211,7 @@ function buildAnimalColumn(entry) {
     }
   });
   columns.appendChild(column);
+  animalListOnLoggedIn.appendChild(column);
 }
 
 // show add animal panel
